3a650ba7ca9a08bdc0464c255266045853b857e9
Hao Hu <hhu94@uw.edu>
2016-08-01 07:45:14 -0700

    Update to use lured_pokemon message type

--- alarms/alarm_manager.py
+++ alarms/alarm_manager.py
@@ -39,15 +39,56 @@ class Alarm_Manager:
 				else:
 					log.info("Alarm not activated: " + alarm['type'] + " because value not set to \"True\"")
 
-	#Send a notification to alarms about a found pokemon
-	def trigger_pkmn(self, pkmn):
-		if 'encounter_id' in pkmn:
-			trigger_normal_pkmn(self, pkmn)
-		elif 'pokestop_id' in pkmn:
-			trigger_lured_pkmn(self, pkmn)
-		else:
-			raise ValueError("Pokemon is not normal nor lured.")
+        def trigger_normal_pkmn(self, pkmn):
+                if pkmn['encounter_id'] not in self.seen:
+                        name = pkmn_name(pkmn['pokemon_id'])
+                        disappear_time = datetime.utcfromtimestamp(pkmn['disappear_time']);
+                        pkinfo = {
+                                'id': pkmn['pokemon_id'],
+                                'name': name,
+                                'alert': pkmn_alert_text(name),
+                                'gmaps_link': gmaps_link(pkmn['latitude'], pkmn['longitude']),
+                                'time_text': pkmn_time_text(disappear_time),
+                                'disappear_time': disappear_time
+                                }
+                        self.seen[pkmn['encounter_id']] = pkinfo
+                        if self.notify_list[name] != "True" :
+                                log.info(name + " notification was not triggered because alarm is disabled.")
+                        elif disappear_time < datetime.utcnow() :
+                                log.info(name + " notification was not triggered because time_left had passed.")
+                        else:
+                                log.info(name + " notication was triggered!")
+                                for alarm in self.alarms:
+                                        alarm.pokemon_alert(pkinfo)
+                        if len(self.seen) > 10000 :
+                                self.clear_stale(normal=True);
 
+        def trigger_lured_pkmn(self, pkmn):
+                disappear_time = datetime.utcfromtimestamp(pkmn['disappear_time']);
+                # Only process if this is a new lured pokestop or if the pokestop's pokemon
+                # has a new disappear time that is greater than the last one.
+                if (pkmn['pokestop_id'] not in self.lured_seen
+                                or self.lured_seen[pkmn['pokestop_id']]['disappear_time'] < disappear_time):
+                        name = pkmn_name(pkmn['pokemon_id'])
+                        pkinfo = {
+                                'id': pkmn['pokemon_id'],
+                                'name': name,
+                                'alert': pkmn_alert_text(name),
+                                'gmaps_link': gmaps_link(pkmn['latitude'], pkmn['longitude']),
+                                'time_text': pkmn_time_text(disappear_time),
+                                'disappear_time': disappear_time
+                                }
+                        self.lured_seen[pkmn['pokestop_id']] = pkinfo
+                        if self.notify_list[name] != "True" :
+                                log.info(name + " notification was not triggered because alarm is disabled.")
+                        elif disappear_time < datetime.utcnow() :
+                                log.info(name + " notification was not triggered because time_left had passed.")
+                        else:
+                                log.info(name + " notication was triggered!")
+                                for alarm in self.alarms:
+                                        alarm.pokemon_alert(pkinfo)
+                        if len(self.lured_seen) > 10000:
+                                self.clear_stale(lured=True);
 
 	#Send a notication about pokemon lure found
 	def notify_lures(self, lures):
@@ -73,54 +114,3 @@ class Alarm_Manager:
 					old.append(id)
 			for id in old:
 				del self.lured_seen[id]
-
-def trigger_normal_pkmn(self, pkmn):
-	if pkmn['encounter_id'] not in self.seen:
-		name = pkmn_name(pkmn['pokemon_id'])
-		disappear_time = datetime.utcfromtimestamp(pkmn['disappear_time']);
-		pkinfo = {
-			'id': pkmn['pokemon_id'],
-			'name': name,
-			'alert': pkmn_alert_text(name),
-			'gmaps_link': gmaps_link(pkmn['latitude'], pkmn['longitude']),
-			'time_text': pkmn_time_text(disappear_time),
-			'disappear_time': disappear_time
-			}
-		self.seen[pkmn['encounter_id']] = pkinfo
-		if self.notify_list[name] != "True" :
-			log.info(name + " notification was not triggered because alarm is disabled.")
-		elif disappear_time < datetime.utcnow() :
-			log.info(name + " notification was not triggered because time_left had passed.")
-		else:
-			log.info(name + " notication was triggered!")
-			for alarm in self.alarms:
-				alarm.pokemon_alert(pkinfo)
-		if len(self.seen) > 10000 :
-			self.clear_stale(normal=True);
-
-def trigger_lured_pkmn(self, pkmn):
-	disappear_time = datetime.utcfromtimestamp(pkmn['disappear_time']);
-	# Only process if this is a new lured pokestop or if the pokestop's pokemon
-	# has a new disappear time that is greater than the last one.
-	if (pkmn['pokestop_id'] not in self.lured_seen
-			or self.lured_seen[pkmn['pokestop_id']]['disappear_time'] < disappear_time):
-		name = pkmn_name(pkmn['pokemon_id'])
-		pkinfo = {
-			'id': pkmn['pokemon_id'],
-			'name': name,
-			'alert': pkmn_alert_text(name),
-			'gmaps_link': gmaps_link(pkmn['latitude'], pkmn['longitude']),
-			'time_text': pkmn_time_text(disappear_time),
-			'disappear_time': disappear_time
-			}
-		self.lured_seen[pkmn['pokestop_id']] = pkinfo
-		if self.notify_list[name] != "True" :
-			log.info(name + " notification was not triggered because alarm is disabled.")
-		elif disappear_time < datetime.utcnow() :
-			log.info(name + " notification was not triggered because time_left had passed.")
-		else:
-			log.info(name + " notication was triggered!")
-			for alarm in self.alarms:
-				alarm.pokemon_alert(pkinfo)
-		if len(self.lured_seen) > 10000:
-			self.clear_stale(lured=True);
