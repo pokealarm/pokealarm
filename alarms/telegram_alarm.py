c0081bd9d9f40a1bb00cfa2a53c533a80b070dd7
VanGent <Kurtis.VanGent@coyotes.usd.edu>
2016-08-07 17:35:23 -0500

    Added some error handling to Telegram.

--- alarms/telegram_alarm.py
+++ alarms/telegram_alarm.py
@@ -9,7 +9,8 @@ log = logging.getLogger(__name__)
 class Telegram_Alarm(Alarm):
  	
 	def __init__(self, settings):
- 		self.client = telepot.Bot(settings['bot_token']) 
+		self.bot_token = settings['bot_token']
+ 		self.client = telepot.Bot(self.bot_token) 
  		self.channel = settings['chat_id']
 		self.header = settings.get('header', "A wild <pkmn> has appeared!")
 		self.body_text = settings.get('body_text', "<gmaps> \n Available until <24h_time> (<time_left>).")
@@ -19,4 +20,17 @@ class Telegram_Alarm(Alarm):
  	def pokemon_alert(self, pkinfo):
 		header = replace(self.header, pkinfo)
 		body_text =  replace(self.body_text, pkinfo)
-		message = self.client.sendMessage(self.channel, '<b>' + header + '</b> \n' + body_text, parse_mode='HTML', disable_web_page_preview='False')
+		for i in range(1,6):
+			try:
+				message = self.client.sendMessage(self.channel, '<b>' + header + '</b> \n' 
+					+ body_text, parse_mode='HTML', disable_web_page_preview='False')
+				break #message sent succesfull
+			except Exception as e:
+				log.error(e)
+				if i < 5 :
+					log.error("Error sending telegram alert. %d attempt of 5." % i)
+					time.sleep(5)
+					self.client = telepot.Bot(self.bot_token)
+				else:
+					log.error("Could not send telegram alert... Giving up.")
+				
