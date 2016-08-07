643b7c97f28ab84c4d495387468cfcf0d8d210ef
VanGent <Kurtis.VanGent@coyotes.usd.edu>
2016-08-07 10:37:24 -0500

    Updated Slack icon URL.

--- alarms/slack_alarm.py
+++ alarms/slack_alarm.py
@@ -32,5 +32,5 @@ class Slack_Alarm(Alarm):
 			channel=self.channel,
 			username=username,
 			text='<{}|{}> {}'.format(link,  notify_text , body_text),
-			icon_url='https://raw.githubusercontent.com/AHAAAAAAA/PokemonGo-Map/master/static/icons/{id:d}.png'.format(**pkinfo)
+			icon_url='https://raw.githubusercontent.com/PokemonGoMap/PokemonGo-Map/master/static/icons/{id}.png'.format(**pkinfo)
 		)
