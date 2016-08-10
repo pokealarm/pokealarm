4ebdee729c4fd73301c30cf9b531d613404bc4e6
brettgus <brett.gustafson@gmail.com>
2016-08-10 17:32:09 -0500

    Code cleanup
    Reduce number of if statements

--- alarms/utils.py
+++ alarms/utils.py
@@ -180,15 +180,11 @@ def point_in_poly(x,y):
         n = len(poly)
         inside = False
 
-        p1x,p1y = poly[0]
-        for i in range(n+1):
-                p2x,p2y = poly[i % n]
-                if y > min(p1y,p2y):
-                        if y <= max(p1y,p2y):
-                                if x <= max(p1x,p2x):
-                                        if p1y != p2y:
-                                                xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
-                                        if p1x == p2x or x <= xints:
-                                                inside = not inside
-                p1x,p1y = p2x,p2y
-        return inside
+	p1x, p1y = poly[0]
+	for i in range(n + 1):
+	p2x, p2y = poly[i % n]
+	if y > min(p1y, p2y) and y <= max(p1y, p2y) and x <= max(p1x, p2x):
+	    if p1x == p2x or x <= (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x:
+	        inside = not inside
+	p1x, p1y = p2x, p2y
+	return inside
