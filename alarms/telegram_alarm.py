4cacbfadc06f0842ee8a9d89b9e5147804ed9f2f
VanGent <Kurtis.VanGent@coyotes.usd.edu>
2016-08-10 12:43:42 -0500

    Added 'send_map' option to Telegram, enabled by default.

--- alarms/telegram_alarm.py
+++ alarms/telegram_alarm.py
@@ -12,6 +12,7 @@ class Telegram_Alarm(Alarm):
 		self.bot_token = settings['bot_token']
  		self.connect()
  		self.chat_id = settings['chat_id']
+		self.send_map = parse_boolean(settings.get('send_map', "True"))
 		self.title = settings.get('title', "A wild <pkmn> has appeared!")
 		self.body = settings.get('body', "<gmaps> \n Available until <24h_time> (<time_left>).")
 		log.info("Telegram Alarm intialized")
@@ -27,7 +28,13 @@ class Telegram_Alarm(Alarm):
 			'chat_id': self.chat_id,
 			'text': '<b>' + title + '</b> \n' + body,
 			'parse_mode': 'HTML',
-			'disable_web_page_preview': 'False'
+			'disable_web_page_preview': 'False',
 		}
 		try_sending(log, self.connect, "Telegram", self.client.sendMessage, args)
-				
+		args = { 
+			'chat_id': self.chat_id,
+			'latitude': pkinfo['lat'],
+			'longitude':  pkinfo['lng']
+		}
+		if self.send_map is True:
+			try_sending(log, self.connect, "Telegram (loc)", self.client.sendLocation, args)
