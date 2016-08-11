250a34691ba32984c7f36cf3c8d11e67032fa9c3
FMKaiba <fmkaiba@gmail.com>
2016-08-10 19:10:32 -0700

    Add ability to filter notifications from lured Pokestops (-sl, --skip_lured)

--- alarms/__init__.py
+++ alarms/__init__.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 
 config = {
-    'LOCALE': 'en',
-    'LOCALES_DIR': 'locales',
-    'ROOT_PATH': '',
+	'LOCALE': 'en',
+	'LOCALES_DIR': 'locales',
+	'ROOT_PATH': '',
 	'HOST':'127.0.0.1',
 	'PORT':'4000',
+	'SKIP_LURED':False
 }
 
 from utils import  *
