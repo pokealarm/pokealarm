a68eb6b4c79cbf67bd4e04ac6fed9379d1196547
Ravi Mehra <javishi@gmail.com>
2016-08-27 22:31:46 +0000

    Added a new CLI option, -tz or --timezone, that sends notifications formatted in that timezone

--- alarms/utils.py
+++ alarms/utils.py
@@ -10,6 +10,7 @@ import time
 import sys
 import re
 import googlemaps
+import pytz
 from glob import glob
 from datetime import datetime, timedelta
 from math import radians, sin, cos, atan2, sqrt
@@ -77,6 +78,7 @@ def set_config(root_path):
 	parser.add_argument('-d', '--debug', help='Debug Mode', action='store_true',  default=False)
 	parser.add_argument('-gf', '--geofence', help='Specify a file of coordinates, limiting alerts to within this area')
 	parser.add_argument('-tl', '--timelimit', type=int, help='Minimum number of seconds remaining on a pokemon to notify', default=0)
+	parser.add_argument('-tz', '--timezone', help='Timezone used for notifications.  Default: UTC', default='UTC')
 	
 	args = parser.parse_args()
 	
@@ -97,6 +99,13 @@ def set_config(root_path):
 	
 	if args.geofence:
 		config['GEOFENCE'] = Geofence(os.path.join(root_path, args.geofence))
+
+	try:
+		config['TIMEZONE'] = pytz.timezone(args.timezone)
+		log.info("Timezone set to: %s" % args.timezone)
+	except pytz.exceptions.UnknownTimeZoneError:
+		log.error("Invalid timezone. For a list of valid timezones, see https://en.wikipedia.org/wiki/List_of_tz_database_time_zones")
+		sys.exit(0)
 		
 	config['REV_LOC'] = False
 	config['DM_WALK'] = False
@@ -203,9 +212,10 @@ def get_timestamps(t):
 	(h, m) = divmod(m, 60)
 	d = timedelta(hours = h, minutes = m, seconds = s)
 	disappear_time = datetime.now() + d
+        disappear_time = disappear_time.replace(tzinfo=pytz.utc).astimezone(config.get("TIMEZONE"))
 	time_left = "%dm %ds" % (m, s)
-	time_12 = disappear_time.strftime("%I:%M") + disappear_time.strftime("%p").lower()
-	time_24 = disappear_time.strftime("%H:%M:%S")
+	time_12 = disappear_time.strftime("%I:%M:%S") + disappear_time.strftime("%p").lower() + disappear_time.strftime(" %Z")
+	time_24 = disappear_time.strftime("%H:%M:%S%Z")
 	return (time_left, time_12, time_24)
 	
 #########################################################################
