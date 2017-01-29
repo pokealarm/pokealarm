5d474a1dec180049938540eb3d3f33ff5a659058
kvangent <kvangent@users.noreply.github.com>
2017-01-28 23:35:37 -0600

    Fixed an issue with locales being off by one

--- start_pokealarm.py
+++ start_pokealarm.py
@@ -141,8 +141,8 @@ def parse_settings(root_path):
     config['QUIET'] = False
     config['DEBUG'] = args.debug
 
-    for list_ in [args.key, args.filters, args.alarms, args.geofences,
-                  args.location, args.units, args.timelimit, args.timezone]:
+    for list_ in [args.key, args.filters, args.alarms, args.geofences, args.location,
+                  args.locale, args.units, args.timelimit, args.timezone]:
         if len(list_) > 1:  # Remove defaults from the list
             list_.pop(0)
         size = len(list_)
