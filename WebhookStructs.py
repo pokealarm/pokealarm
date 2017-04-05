61e45f5e9c40b91ae4278a584e6a0c52aa9d7a0c
Avatar690 <jeremy.simmerman@gmail.com>
2017-04-04 21:51:17 -0500

    Content patch
    For Discord only, allows user to input custom text (or role/user mentions) into the JSON payload.

---
+++ WebhookStructs.py
@@ -0,0 +1,125 @@
+# Standard Library Imports
+from datetime import datetime
+import logging
+import traceback
+# 3rd Party Imports
+# Local Imports
+from Utils import get_gmaps_link, get_move_damage, get_move_dps, get_move_duration,\
+    get_move_energy, get_pokemon_gender, get_pokemon_size, get_applemaps_link
+
+log = logging.getLogger('WebhookStructs')
+
+
+################################################## Webhook Standards  ##################################################
+
+
+# RocketMap Standards
+class RocketMap:
+    def __init__(self):
+        raise NotImplementedError("This is a static class not meant to be initiated")
+
+    @staticmethod
+    def make_object(data):
+        try:
+            kind = data.get('type')
+            if kind == 'pokemon':
+                return RocketMap.pokemon(data.get('message'))
+            elif kind == 'pokestop':
+                return RocketMap.pokestop(data.get('message'))
+            elif kind == 'gym' or kind == 'gym_details':
+                return RocketMap.gym(data.get('message'))
+            elif kind in ['captcha', 'scheduler']:  # Unsupported Webhooks
+                log.debug("{} webhook received. This webhooks is not yet supported at this time.".format({kind}))
+            else:
+                log.error("Invalid type specified ({}). Are you using the correct map type?".format(kind))
+        except Exception as e:
+            log.error("Encountered error while processing webhook ({}: {})".format(type(e).__name__, e))
+            log.debug("Stack trace: \n {}".format(traceback.format_exc()))
+        return None
+
+    @staticmethod
+    def pokemon(data):
+        log.debug("Converting to pokemon: \n {}".format(data))
+        # Get some stuff ahead of time (cause we are lazy)
+        quick_id = check_for_none(int, data.get('move_1'), '?')
+        charge_id = check_for_none(int, data.get('move_2'), '?')
+        lat, lng = data['latitude'], data['longitude']
+        # Generate all the non-manager specifi
+        pkmn = {
+            'type': "pokemon",
+            'id': data['encounter_id'],
+            'pkmn_id': int(data['pokemon_id']),
+            'disappear_time': datetime.utcfromtimestamp(data['disappear_time']),
+            'lat': float(data['latitude']),
+            'lng': float(data['longitude']),
+            'iv': '?',
+            'atk': check_for_none(int, data.get('individual_attack'), '?'),
+            'def': check_for_none(int, data.get('individual_defense'), '?'),
+            'sta': check_for_none(int, data.get('individual_stamina'), '?'),
+            'quick_id': quick_id,
+            'quick_damage': get_move_damage(quick_id),
+            'quick_dps': get_move_dps(quick_id),
+            'quick_duration': get_move_duration(quick_id),
+            'quick_energy': get_move_energy(quick_id),
+            'charge_id': charge_id,
+            'charge_damage': get_move_damage(charge_id),
+            'charge_dps': get_move_dps(charge_id),
+            'charge_duration': get_move_duration(charge_id),
+            'charge_energy': get_move_energy(charge_id),
+            'height': check_for_none(float, data.get('height'), 'unkn'),
+            'weight': check_for_none(float, data.get('weight'), 'unkn'),
+            'gender': get_pokemon_gender(check_for_none(int, data.get('gender'), '?')),
+            'size': 'unknown',
+            'gmaps': get_gmaps_link(lat, lng),
+            'applemaps': get_applemaps_link(lat, lng),
+            'content': None #Avatar
+        }
+        if pkmn['atk'] != '?' or pkmn['def'] != '?' or pkmn['sta'] != '?':
+            pkmn['iv'] = float(((pkmn['atk'] + pkmn['def'] + pkmn['sta']) * 100) / float(45))
+        else:
+            pkmn['atk'], pkmn['def'], pkmn['sta'] = '?', '?', '?'
+
+        if pkmn['height'] != 'unkn' or pkmn['weight'] != 'unkn':
+            pkmn['size'] = get_pokemon_size(pkmn['pkmn_id'], pkmn['height'], pkmn['weight'])
+            pkmn['height'] = "{:.2f}".format(pkmn['height'])
+            pkmn['weight'] = "{:.2f}".format(pkmn['weight'])
+
+        return pkmn
+
+    @staticmethod
+    def pokestop(data):
+        log.debug("Converting to pokestop: \n {}".format(data))
+        if data.get('lure_expiration') is None:
+            log.debug("Un-lured pokestop... ignoring.")
+            return None
+        stop = {
+            'type': "pokestop",
+            'id': data['pokestop_id'],
+            'expire_time':  datetime.utcfromtimestamp(data['lure_expiration']),
+            'lat': float(data['latitude']),
+            'lng': float(data['longitude'])
+        }
+        stop['gmaps'] = get_gmaps_link(stop['lat'], stop['lng'])
+        return stop
+
+    @staticmethod
+    def gym(data):
+        log.debug("Converting to gym: \n {}".format(data))
+        gym = {
+            'type': "gym",
+            'id': data.get('gym_id',  data.get('id')),
+            "team_id": int(data.get('team_id',  data.get('team'))),
+            "points": str(data.get('gym_points')),
+            "guard_pkmn_id": data.get('guard_pokemon_id'),
+            'lat': float(data['latitude']),
+            'lng': float(data['longitude'])
+        }
+        gym['gmaps'] = get_gmaps_link(gym['lat'], gym['lng'])
+        return gym
+
+
+# Ensure that the value isn't None but replacing with a default
+def check_for_none(type_, val, default):
+    return type_(val) if val is not None else default
+
+########################################################################################################################
