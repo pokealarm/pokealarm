bbba098d0f3e03d1a05fcdbdfb789b1e5fa42e8d
walaoaaa1234 <walaoaaa1234@hotmail.com>
2017-04-20 01:03:19 +0800

    mistake 0.2

--- PokeAlarm/Manager.py
+++ PokeAlarm/Manager.py
@@ -618,7 +618,7 @@ class Manager(object):
             if 'gym_name' in gym:
                 gym_name = gym['gym_name']
                 for pokemon in gym['pokemon']:
-                    defenders += '{} (CP {}) trained by {} ({})\n'.format(self.__pokemon_name(pokemon['pokemon_id']), pokemon['cp'], pokemon['trainer_name'], pokemon['trainer_level'])
+                    defenders += '{} (CP {}) trained by {} ({})\n'.format(self.__pokemon_name[pokemon['pokemon_id']], pokemon['cp'], pokemon['trainer_name'], pokemon['trainer_level'])
             else:
                 log.debug("'gym' hook ignored, waiting for 'gym_details'")
                 return
