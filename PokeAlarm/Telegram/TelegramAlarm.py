19caea1767002319cc288c46e2e81e13eb8027d4
Ricardo <ricardo.grandon@gmail.com>
2017-05-01 01:10:46 -0300

    Update TelegramAlarm.py

--- PokeAlarm/Telegram/TelegramAlarm.py
+++ PokeAlarm/Telegram/TelegramAlarm.py
@@ -106,7 +106,10 @@ class TelegramAlarm(Alarm):
     # Trigger an alert based on Pokemon info
     def pokemon_alert(self, pokemon_info):
         if self.__pokemon['stickers']:
-            self.send_alert(self.__pokemon, pokemon_info, sticker_list.get(str(pokemon_info['pkmn_id'])))
+            if (pokemon_info['pkmn_id'] == '201')
+                self.send_alert(self.__pokemon, pokemon_info, unown_list.get(str(pokemon_info['pkmn_form'])))
+            else:
+                self.send_alert(self.__pokemon, pokemon_info, sticker_list.get(str(pokemon_info['pkmn_id'])))
         else:
             self.send_alert(self.__pokemon, pokemon_info)
 
