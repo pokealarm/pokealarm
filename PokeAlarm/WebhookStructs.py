97d9569137f87c98755b0a1e14a7e9f61e83f6ef
Ricardo <ricardo.grandon@gmail.com>
2017-05-07 00:22:16 -0300

    Update WebhookStructs.py

--- PokeAlarm/WebhookStructs.py
+++ PokeAlarm/WebhookStructs.py
@@ -53,7 +53,7 @@ class RocketMap:
             'lat': float(data['latitude']),
             'lng': float(data['longitude']),
             'cp': check_for_none(int, data.get('cp'), '?'),
-            'level': check_for_none(int, data.get('pokemon_level'), '?'),
+            'level': 2,
             'iv': '?',
             'atk': check_for_none(int, data.get('individual_attack'), '?'),
             'def': check_for_none(int, data.get('individual_defense'), '?'),
