a494b2006855d45641b31f4047864f224e0a2482
kvangent <kurtis.vangent@coyotes.usd.edu>
2017-11-18 14:41:04 -0800

    Fixed some PEP8 mistakes in Alarms module init.

--- PokeAlarm/Alarms/__init__.py
+++ PokeAlarm/Alarms/__init__.py
@@ -3,7 +3,8 @@ from PokeAlarm.Utils import require_and_remove_key
 
 
 def alarm_factory(settings, max_attempts, api_key):
-    kind = require_and_remove_key('type', settings, "Alarm objects in Alarms file.")
+    kind = require_and_remove_key(
+        'type', settings, "Alarm objects in Alarms file.")
     if kind == 'discord':
         from PokeAlarm.Alarms.Discord import DiscordAlarm
         return DiscordAlarm(settings, max_attempts, api_key)
@@ -26,4 +27,4 @@ def alarm_factory(settings, max_attempts, api_key):
         from PokeAlarm.Alarms.Twitter import TwitterAlarm
         return TwitterAlarm(settings)
     else:
-        raise ValueError("%s is not a valid cache type!".format(kind))
+        raise ValueError("%s is not a valid alarm type!".format(kind))
