a2a94e2931a7921c4b34c9ae056ec023629a8c39
sLoPPydrive <sloppydrive@gmail.com>
2017-11-22 21:50:57 +0100

    Merge --log_to_file and --log_path to --log <filename>; add sanity checks

--- start_pokealarm.py
+++ start_pokealarm.py
@@ -19,7 +19,6 @@ import Queue
 import json
 import os
 import sys
-from time import strftime
 # 3rd Party Imports
 from flask import Flask, request, abort
 # Local Imports
@@ -174,25 +173,13 @@ def parse_settings(root_path):
     parser.add_argument(
         '-tz', '--timezone', type=str, action='append', default=[None],
         help='Timezone used for notifications. Ex: "America/Los_Angeles"')
-    parser.add_argument('--log_to_file',
-                        help=('Enable logging to file.'),
-                        action='store_true', default=False)
-    parser.add_argument('--log_path',
-                        help=('Defines directory to save log files to.'),
-                        default='logs/')
+    parser.add_argument(
+        '--log', help='Log output to given file.')
 
     args = parser.parse_args()
 
-    if args.log_to_file:
-        # Create directory for log files.
-        if not os.path.exists(args.log_path):
-            os.mkdir(args.log_path)
-        date = strftime('%Y%m%d_%H%M')
-        filename = os.path.join(args.log_path, '{}.log'.format(date))
-        fh = logging.FileHandler(filename)
-        fh.setFormatter(
-            logging.Formatter(fmt='%(asctime)s [%(processName)15.15s][%(name)10.10s][%(levelname)8.8s] %(message)s'))
-        logging.root.addHandler(fh)
+    if args.log:
+        configure_file_logging(args.log)
 
     if args.debug:
         log.setLevel(logging.DEBUG)
@@ -272,6 +259,23 @@ def parse_settings(root_path):
     # Set up signal handlers for graceful exit
     signal(signal.SIGINT, exit_gracefully)
     signal(signal.SIGTERM, exit_gracefully)
+
+
+def configure_file_logging(log_file):
+    path, filename = os.path.split(log_file)
+    if not os.path.exists(path):
+        try:
+            os.makedirs(path)
+        except OSError:
+            log.exception("Could not enable logging to file.")
+            return
+    if os.access(path, os.W_OK):
+        fh = logging.FileHandler(log_file)
+        fh.setFormatter(
+            logging.Formatter(
+                fmt='%(asctime)s [%(processName)15.15s][%(name)10.10s]'
+                    + '[%(levelname)8.8s] %(message)s'))
+        logging.root.addHandler(fh)
 
 
 # Because lists are dumb and don't have a failsafe get
