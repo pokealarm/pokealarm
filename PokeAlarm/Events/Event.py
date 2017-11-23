edf3d217bdec17614b73f62b2b7d716525a73e40
kvangent <kurtis.vangent@coyotes.usd.edu>
2017-11-22 22:15:38 -0800

    Added Monster Event Class.

---
+++ PokeAlarm/Events/Event.py
@@ -0,0 +1,26 @@
+# Standard Library Imports
+import logging
+# 3rd Party Imports
+# Local Imports
+from PokeAlarm import Unknown
+
+
+class Event(object):
+    """ Abstract class representing details related to different events. """
+
+    unknown_set = {
+        Unknown.TINY, Unknown.SMALL, Unknown.REGULAR
+    }
+
+    def __init__(self, kind):
+        """ Initializes the logger for the event. """
+        self._log = logging.getLogger(kind)
+
+    def generate_dts(self, locale):
+        """ Return a dict with all the DTS for this events. """
+        raise NotImplementedError("This is an abstract method.")
+
+    @classmethod
+    def check_for_none(cls, cast, val, default):
+        """ Returns val as type cast or default if val is None """
+        return cast(val) if val is not None else default
