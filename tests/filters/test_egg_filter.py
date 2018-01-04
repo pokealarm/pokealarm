54cd4e48fc3bd3dc85aab66eb742b0513ae02899
BillyJBryant <3013565+billyjbryant@users.noreply.github.com>
2018-01-04 00:29:23 -0500

    Updated tests and added custom_dts and missing_info testing

--- tests/filters/test_egg_filter.py
+++ tests/filters/test_egg_filter.py
@@ -13,45 +13,23 @@ class TestEggFilter(unittest.TestCase):
 
     def test_egg_lvl(self):
         # Create the filters
-        settings = {"min_egg_lvl": 3, "max_egg_lvl": 5}
+        settings = {"min_egg_lvl": 2, "max_egg_lvl": 4}
         egg_filter = Filters.EggFilter('filter1', settings)
 
         # Generate events that should pass
-        pass1 = Events.EggEvent(generate_egg({"level": 3}))
-        pass2 = Events.EggEvent(generate_egg({"level": 4}))
-        pass3 = Events.EggEvent(generate_egg({"level": 5}))
+        pass1 = Events.EggEvent(generate_egg({"level": 2}))
+        pass2 = Events.EggEvent(generate_egg({"level": 3}))
+        pass3 = Events.EggEvent(generate_egg({"level": 4}))
         # Test passing events
         for e in [pass1, pass2, pass3]:
             self.assertTrue(egg_filter.check_event(e))
 
         # Generate events that should fail
         fail1 = Events.EggEvent(generate_egg({"level": 1}))
-        fail2 = Events.EggEvent(generate_egg({"level": 2}))
+        fail2 = Events.EggEvent(generate_egg({"level": 5}))
 
         # Test failing events
         for e in [fail1, fail2]:
-            self.assertFalse(egg_filter.check_event(e))
-
-    def test_egg_dist(self):
-        # Create the filters
-        settings = {"min_dist": 200, "max_dist": 500}
-        egg_filter = Filters.EggFilter('filter1', settings)
-
-        # Generate events that should pass
-        pass1 = Events.EggEvent(generate_egg({"distance": 200}))
-        pass2 = Events.EggEvent(generate_egg({"distance": 350}))
-        pass3 = Events.EggEvent(generate_egg({"distance": 500}))
-        # Test passing events
-        for e in [pass1, pass2, pass3]:
-            self.assertTrue(egg_filter.check_event(e))
-
-        # Generate events that should fail
-        fail1 = Events.EggEvent(generate_egg({"distance": 100}))
-        fail2 = Events.EggEvent(generate_egg({"distance": 199}))
-        fail3 = Events.EggEvent(generate_egg({"distance": 501}))
-
-        # Test failing events
-        for e in [fail1, fail2, fail3]:
             self.assertFalse(egg_filter.check_event(e))
 
     def test_gym_names(self):
@@ -95,6 +73,28 @@ class TestEggFilter(unittest.TestCase):
         # Test failing events
         for e in [fail1]:
             self.assertFalse(egg_filter.check_event(e))
+
+    def test_missing_info(self):
+        # Create the filters
+        settings = {"missing_info": True}
+        egg_filter = Filters.EggFilter('filter1', settings)
+
+        # Generate events that should pass
+        pass1 = Events.EggEvent(generate_egg({}))
+        # Test passing events
+        for e in [pass1]:
+            self.assertTrue(egg_filter.check_event(e))
+
+    def test_custom_dts(self):
+        # Create the filters
+        settings = {"custom_dts": {"key1": "pass1"}}
+        egg_filter = Filters.EggFilter('filter1', settings)
+
+        # Generate events that should pass
+        pass1 = Events.EggEvent(generate_egg({}))
+        # Test passing events
+        for e in [pass1]:
+            self.assertTrue(egg_filter.check_event(e))
 
 
 # Create a generic egg, overriding with an specific values
