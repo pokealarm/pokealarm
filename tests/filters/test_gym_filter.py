101293cea5d99ce070b5be81aeb19e64407ed5f0
BillyJBryant <3013565+billyjbryant@users.noreply.github.com>
2018-01-05 01:35:49 -0500

    Pushing slots_available/guard_count DTS + min/max_slots Filters

--- tests/filters/test_gym_filter.py
+++ tests/filters/test_gym_filter.py
@@ -17,17 +17,39 @@ class TestGymFilter(unittest.TestCase):
         gym_filter = Filters.GymFilter('filter1', settings)
 
         # Generate events that should pass
-        pass1 = Events.GymEvent(generate_generic_gym({"name": "pass1"}))
-        pass2 = Events.GymEvent(generate_generic_gym({"name": "pass2"}))
-        pass3 = Events.GymEvent(generate_generic_gym({"name": "pass3"}))
+        pass1 = Events.GymEvent(generate_gym({"name": "pass1"}))
+        pass2 = Events.GymEvent(generate_gym({"name": "pass2"}))
+        pass3 = Events.GymEvent(generate_gym({"name": "pass3"}))
         # Test passing events
         for e in [pass1, pass2, pass3]:
             self.assertTrue(gym_filter.check_event(e))
 
         # Generate events that should fail
-        fail1 = Events.GymEvent(generate_generic_gym({"name": "fail1"}))
-        fail2 = Events.GymEvent(generate_generic_gym({"name": "failpass"}))
-        fail3 = Events.GymEvent(generate_generic_gym({"name": "passfail"}))
+        fail1 = Events.GymEvent(generate_gym({"name": "fail1"}))
+        fail2 = Events.GymEvent(generate_gym({"name": "failpass"}))
+        fail3 = Events.GymEvent(generate_gym({"name": "passfail"}))
+
+        # Test failing events
+        for e in [fail1, fail2, fail3]:
+            self.assertFalse(gym_filter.check_event(e))
+
+    def test_gym_guards(self):
+        # Create the filters
+        settings = {"min_slots": 2, "max_slots": 4}
+        gym_filter = Filters.GymFilter('filter1', settings)
+
+        # Generate events that should pass
+        pass1 = Events.GymEvent(generate_gym({"slots_available": 2}))
+        pass2 = Events.GymEvent(generate_gym({"slots_available": 3}))
+        pass3 = Events.GymEvent(generate_gym({"slots_available": 4}))
+        # Test passing events
+        for e in [pass1, pass2, pass3]:
+            self.assertTrue(gym_filter.check_event(e))
+
+        # Generate events that should fail
+        fail1 = Events.GymEvent(generate_gym({"slots_available": 0}))
+        fail2 = Events.GymEvent(generate_gym({"slots_available": 1}))
+        fail3 = Events.GymEvent(generate_gym({"slots_available": 5}))
 
         # Test failing events
         for e in [fail1, fail2, fail3]:
@@ -35,7 +57,7 @@ class TestGymFilter(unittest.TestCase):
 
 
 # Create a generic gym, overriding with an specific values
-def generate_generic_gym(values):
+def generate_gym(values):
     gym = {
         "id": "OWNmOTFmMmM0YTY3NGQwYjg0Y2I1N2JlZjU4OWRkMTYuMTY=",
         "url": "???",
