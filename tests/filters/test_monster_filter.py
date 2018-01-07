5234405f17df416a8140403062bbafc46f44ee4f
LegitDongo <9087537+LegitDongo@users.noreply.github.com>
2018-01-07 16:37:18 -0600

    Add CP unit tests for Monsters and Raids.

--- tests/filters/test_monster_filter.py
+++ tests/filters/test_monster_filter.py
@@ -283,6 +283,15 @@ class TestMonsterFilter(unittest.TestCase):
         })))
         self.assertFalse(mon_filter.check_event(create_event({})))
 
+    def test_cp(self):
+        settings = {'min_cp': 20, 'max_cp': 500}
+        mon_filter = Filters.MonFilter('cp_filter', settings)
+        for i in [20, 250, 500]:
+            self.assertTrue(mon_filter.check_event(create_event({'cp': i})))
+
+        for i in [19, 501, 9999]:
+            self.assertFalse(mon_filter.check_event(create_event({'cp': i})))
+
 
 # Create a generic monster, overriding with an specific values
 def generate_monster(values):
