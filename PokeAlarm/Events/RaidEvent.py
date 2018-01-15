6f7f6fa89291b1ec770f9be67c85991d46e00821
Marcus Ponting <m-ponting@sky.com>
2018-01-15 19:51:12 +0000

    Add weather id and emoji

--- PokeAlarm/Events/RaidEvent.py
+++ PokeAlarm/Events/RaidEvent.py
@@ -7,7 +7,7 @@ from . import BaseEvent
 from PokeAlarm.Utils import get_gmaps_link, get_applemaps_link, \
     get_time_as_str, get_move_damage, get_move_dps, get_move_duration, \
     get_move_energy, get_dist_as_str, get_pokemon_cp_range, \
-    is_raid_boss_weather_boosted
+    is_raid_boss_weather_boosted, get_weather_emoji
 
 
 class RaidEvent(BaseEvent):
@@ -31,7 +31,7 @@ class RaidEvent(BaseEvent):
         self.distance = Unknown.SMALL  # Completed by Manager
         self.direction = Unknown.TINY  # Completed by Manager
         self.weather_id = check_for_none(
-            int, data.get('weather'), Unknown.REGULAR)
+            int, data.get('weather'), Unknown.TINY)
 
         # Monster Info
         self.raid_lvl = int(data['level'])
@@ -99,7 +99,9 @@ class RaidEvent(BaseEvent):
             'gmaps': get_gmaps_link(self.lat, self.lng),
             'applemaps': get_applemaps_link(self.lat, self.lng),
             'geofence': self.geofence,
+            'weather_id': self.weather_id,
             'weather': locale.get_weather_name(boosted_weather),
+            'weather_emoji': get_weather_emoji(self.weather_id),
 
             # Raid Info
             'raid_lvl': self.raid_lvl,
--- PokeAlarm/Utils.py
+++ PokeAlarm/Utils.py
@@ -304,6 +304,18 @@ def is_raid_boss_weather_boosted(pokemon_id, weather_id):
     return types['type1'] in boosted_types or types['type2'] in boosted_types
 
 
+def get_weather_emoji(weather_id):
+    return {
+        1: '<:sunny:394513855205736448>',
+        2: '<:cloud_rain:394513988215635980>',
+        3: '<:partly_sunny:394514097477124096>',
+        4: '<:cloud:394514210513616896>',
+        5: '<:wind_blowing_face:394514294668263435>',
+        6: '<:snowman:394514420610498560>',
+        7: '<:foggy:394514537916661760>',
+    }.get(weather_id, '?')
+
+
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GMAPS API UTILITIES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
