69953ee9a9d39776ed5220ea3afa8696eff42201
kvangent <kurtis.vangent@coyotes.usd.edu>
2018-01-14 22:09:26 -0800

    Clean up Telegram creation.

--- PokeAlarm/Alarms/Telegram/TelegramAlarm.py
+++ PokeAlarm/Alarms/Telegram/TelegramAlarm.py
@@ -8,13 +8,14 @@ import telepot
 # Local Imports
 from PokeAlarm.Alarms import Alarm
 from Stickers import sticker_list
-from PokeAlarm.Utils import parse_boolean, require_and_remove_key, \
-    reject_leftover_parameters
+from PokeAlarm.Utilities import GenUtils as utils
+from PokeAlarm.Utils import require_and_remove_key
 
 log = logging.getLogger('Telegram')
+
+# 2 lazy 2 type
 try_sending = Alarm.try_sending
 replace = Alarm.replace
-
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ATTENTION! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #             ONLY EDIT THIS FILE IF YOU KNOW WHAT YOU ARE DOING!
@@ -24,95 +25,129 @@ replace = Alarm.replace
 
 
 class TelegramAlarm(Alarm):
-    _defaults = {
-        'pokemon': {
-            # 'chat_id': If no default, required
-            'title': "A wild <mon_name> has appeared!",
-            'body': "Available until <24h_time> (<time_left>)."
+
+    class Alert(object):
+        """ Class that defines the settings for each alert."""
+
+        def __init__(self, kind, data, alert_defaults):
+            default = TelegramAlarm._defaults[kind]
+            default.update(alert_defaults)
+            settings = Alarm.pop_type(data, kind, dict, {})
+
+            self.stickers = Alarm.pop_type(
+                settings, 'stickers', utils.parse_bool, default['stickers'])
+            self.stickers_notify = Alarm.pop_type(
+                settings, 'stickers_notify', utils.parse_bool,
+                default['stickers_notify'])
+            self.message = Alarm.pop_type(
+                settings, 'message', unicode, default['message'])
+            self.message_notify = Alarm.pop_type(
+                settings, 'message_notify', utils.parse_bool,
+                default['message_notify'])
+            self.venue = Alarm.pop_type(
+                settings, 'venue', utils.parse_bool, default['venue'])
+            self.venue_notify = Alarm.pop_type(
+                settings, 'venue_notify', utils.parse_bool,
+                default['venue_notify'])
+            self.map = Alarm.pop_type(
+                settings, 'map', utils.parse_bool, default['map'])
+            self.map_notification = Alarm.pop_type(
+                settings, 'map_notify', utils.parse_bool,
+                default['map_notify'])
+
+            # Reject leftover parameters
+            for key in settings:
+                raise ValueError(
+                    "'{}' is not a recognized parameter for the Alert"
+                    " level in a Telegram Alarm".format(key))
+
+    _defaults = {  # No touchy!!! Edit alarms.json!
+        'monsters': {
+            'message': "__**A wild <mon_name> has appeared!**__\n"
+                       "Available until <24h_time> (<time_left>)."
         },
-        'pokestop': {
-            # 'chat_id': If no default, required
-            'title': "Someone has placed a lure on a Pokestop!",
-            'body': "Lure will expire at <24h_time> (<time_left>)."
+        'stops': {
+            'message': "__**Someone has placed a lure on a Pokestop!**__\n"
+                       "Lure will expire at <24h_time> (<time_left>)."
         },
-        'gym': {
-            # 'chat_id': If no default, required
-            'title': "A Team <old_team> gym has fallen!",
-            'body': "It is now controlled by <new_team>."
+        'gyms': {
+            'message': "__**A Team <old_team> gym has fallen!**__\n"
+                       "It is now controlled by <new_team>."
         },
-        'egg': {
-            'title': "A level <egg_lvl> raid is incoming!",
-            'body': "The egg will hatch <24h_hatch_time> (<hatch_time_left>)."
+        'eggs': {
+            'message': "__**A level <egg_lvl> raid is incoming!**__\n"
+                       "The egg will hatch <24h_hatch_time> "
+                       "(<hatch_time_left>)."
         },
-        'raid': {
-            'title': "A raid is available against <mon_name>!",
-            'body': "The raid is available until <24h_raid_end> "
-                    "(<raid_time_left>)."
+        'raids': {
+            'message': "__**A raid is available against <mon_name>!**__\n"
+                       "The raid is available until <24h_raid_end> "
+                       "(<raid_time_left>)."
         }
     }
 
     # Gather settings and create alarm
     def __init__(self, settings):
         # Required Parameters
-        self.__bot_token = require_and_remove_key(
+        self._bot_token = require_and_remove_key(
             'bot_token', settings, "'Telegram' type alarms.")
-        self.__chat_id = require_and_remove_key(
+        self._chat_id = require_and_remove_key(
             'chat_id', settings, "'Telegram' type alarms.")
-        self.__client = None
 
-        # Optional Alarm Parameters
-        self.__venue = parse_boolean(settings.pop('venue', "False"))
-        self.__location = parse_boolean(settings.pop('location', "True"))
-        self.__stickers = parse_boolean(settings.pop('stickers', 'True'))
-        self.__disable_map_notification = parse_boolean(
-            settings.pop('disable_map_notification', "True"))
-        self.__startup_message = parse_boolean(
-            settings.pop('startup_message', "True"))
+        self._startup_message = self.pop_type(
+            settings, 'startup_message', utils.parse_bool, True)
 
         # Optional Alert Parameters
-        self.__pokemon = self.create_alert_settings(
-            settings.pop('pokemon', {}), self._defaults['pokemon'])
-        self.__pokestop = self.create_alert_settings(
-            settings.pop('pokestop', {}), self._defaults['pokestop'])
-        self.__gym = self.create_alert_settings(
-            settings.pop('gym', {}), self._defaults['gym'])
-        self.__egg = self.create_alert_settings(
-            settings.pop('egg', {}), self._defaults['egg'])
-        self.__raid = self.create_alert_settings(
-            settings.pop('raid', {}), self._defaults['raid'])
+        alert_defaults = {
+            'bot_token': self._bot_token,
+            'chat_id': self._chat_id,
+            'stickers': self.pop_type(
+                settings, 'stickers', utils.parse_bool, True),
+            'stickers_notify': self.pop_type(
+                settings, 'stickers_notify', utils.parse_bool, False),
+            'message_notify': self.pop_type(
+                settings, 'message_notify', utils.parse_bool, True),
+            'venue': self.pop_type(
+                settings, 'venue', utils.parse_bool, False),
+            'venue_notify': self.pop_type(
+                settings, 'venue_notify', utils.parse_bool, True),
+            'map': self.pop_type(
+                settings, 'map', utils.parse_bool, False),
+            'map_notify': self.pop_type(
+                settings, 'map_notify', utils.parse_bool, False),
+            'max_retries': self.pop_type(
+                settings, 'max_retries', int, 3),
+        }
 
-        #  Warn user about leftover parameters
-        reject_leftover_parameters(settings, "'Alarm level in Telegram alarm.")
+        # Alert Settings
+        self._mon_alert = TelegramAlarm.Alert(
+            'monsters', settings, alert_defaults)
+        self._stop_alert = TelegramAlarm.Alert(
+            'stops', settings, alert_defaults)
+        self._gym_alert = TelegramAlarm.Alert(
+            'gyms', settings, alert_defaults)
+        self._egg_alert = TelegramAlarm.Alert(
+            'eggs', settings, alert_defaults)
+        self._raid_alert = TelegramAlarm.Alert(
+            'raids', settings, alert_defaults)
+
+        # Reject leftover parameters
+        for key in settings:
+            raise ValueError("'{}' is not a recognized parameter for the Alarm"
+                             " level in a Telegram Alarm".format(key))
 
         log.info("Telegram Alarm has been created!")
 
     # (Re)establishes Telegram connection
     def connect(self):
-        self.__client = telepot.Bot(self.__bot_token)
+        pass
 
     # Sends a start up message on Telegram
     def startup_message(self):
-        if self.__startup_message:
-            self.send_message(self.__chat_id, "PokeAlarm activated!")
+        if self._startup_message:
+            self.send_message_new(
+                self._bot_token, self._chat_id, "PokeAlarm activated!")
             log.info("Startup message sent!")
-
-    # Set the appropriate settings for each alert
-    def create_alert_settings(self, settings, default):
-        alert = {
-            'chat_id': settings.pop('chat_id', self.__chat_id),
-            'title': settings.pop('title', default['title']),
-            'body': settings.pop('body', default['body']),
-            'venue': parse_boolean(settings.pop('venue', self.__venue)),
-            'location': parse_boolean(
-                settings.pop('location', self.__location)),
-            'disable_map_notification': parse_boolean(
-                settings.pop('disable_map_notification',
-                             self.__disable_map_notification)),
-            'stickers': parse_boolean(settings.pop(
-                'stickers', self.__stickers))
-        }
-        reject_leftover_parameters(settings, "'Alert level in Telegram alarm.")
-        return alert
 
     # Send Alert to Telegram
     def send_alert(self, alert, info, sticker_id=None):
@@ -131,43 +166,43 @@ class TelegramAlarm(Alarm):
 
     # Trigger an alert based on Pokemon info
     def pokemon_alert(self, pokemon_info):
-        if self.__pokemon['stickers']:
-            self.send_alert(self.__pokemon, pokemon_info,
+        if self._mon_alert['stickers']:
+            self.send_alert(self._mon_alert, pokemon_info,
                             sticker_list.get(str(pokemon_info['mon_id'])))
         else:
-            self.send_alert(self.__pokemon, pokemon_info)
+            self.send_alert(self._mon_alert, pokemon_info)
 
     # Trigger an alert based on Pokestop info
     def pokestop_alert(self, pokestop_info):
-        if self.__pokestop['stickers']:
-            self.send_alert(self.__pokestop, pokestop_info,
+        if self._stop_alert['stickers']:
+            self.send_alert(self._stop_alert, pokestop_info,
                             sticker_list.get('pokestop'))
         else:
-            self.send_alert(self.__pokestop, pokestop_info)
+            self.send_alert(self._stop_alert, pokestop_info)
 
     # Trigger an alert based on Pokestop info
     def gym_alert(self, gym_info):
-        if self.__gym['stickers']:
-            self.send_alert(self.__gym, gym_info, sticker_list.get(
+        if self._gym_alert['stickers']:
+            self.send_alert(self._gym_alert, gym_info, sticker_list.get(
                 "team{}".format(gym_info['new_team_id'])))
         else:
-            self.send_alert(self.__gym, gym_info)
+            self.send_alert(self._gym_alert, gym_info)
 
     # Trigger an alert when a raid egg has spawned (UPCOMING raid event)
     def raid_egg_alert(self, raid_info):
-        if self.__raid['stickers'] and raid_info['egg_lvl'] > 0:
-            self.send_alert(self.__egg, raid_info, sticker_list.get(
+        if self._raid_alert['stickers'] and raid_info['egg_lvl'] > 0:
+            self.send_alert(self._egg_alert, raid_info, sticker_list.get(
                 'raid_level_{}'.format(raid_info['egg_lvl'])))
         else:
-            self.send_alert(self.__egg, raid_info)
+            self.send_alert(self._egg_alert, raid_info)
 
     # Trigger an alert based on Raid info
     def raid_alert(self, raid_info):
-        if self.__raid['stickers'] and raid_info['mon_id'] > 0:
-            self.send_alert(self.__raid, raid_info, sticker_list.get(
+        if self._raid_alert['stickers'] and raid_info['mon_id'] > 0:
+            self.send_alert(self._raid_alert, raid_info, sticker_list.get(
                 str(raid_info['mon_id'])))
         else:
-            self.send_alert(self.__raid, raid_info)
+            self.send_alert(self._raid_alert, raid_info)
 
     # Send a message to telegram
     def send_message(self, chat_id, text):
@@ -219,7 +254,7 @@ class TelegramAlarm(Alarm):
     def send_sticker_new(self, bot_token, chat_id, sticker_id, max_attempts):
         args = {
             'url':
-                "https://api.telegram.org/bot%s/sendSticker".format(bot_token),
+                "https://api.telegram.org/bot{}/sendSticker".format(bot_token),
             'payload': {
                 'chat_id': chat_id,
                 'sticker_id': sticker_id,
@@ -230,10 +265,10 @@ class TelegramAlarm(Alarm):
             log, self.connect, "Telegram (STKR)", self.send_webhook, args,
             max_attempts)
 
-    def send_message_new(self, bot_token, chat_id, message, max_attempts):
+    def send_message_new(self, bot_token, chat_id, message, max_attempts=3):
         args = {
             'url':
-                "https://api.telegram.org/bot%s/sendMessage".format(bot_token),
+                "https://api.telegram.org/bot{}/sendMessage".format(bot_token),
             'payload': {
                 'chat_id': chat_id,
                 'text': message,
@@ -248,7 +283,7 @@ class TelegramAlarm(Alarm):
 
     def send_location_new(self, bot_token, chat_id, lat, lng, max_attempts):
         args = {
-            'url': "https://api.telegram.org/bot%s/sendLocation".format(
+            'url': "https://api.telegram.org/bot{}/sendLocation".format(
                     bot_token),
             'payload': {
                 'chat_id': chat_id,
@@ -263,7 +298,7 @@ class TelegramAlarm(Alarm):
 
     def send_venue_new(self, bot_token, chat_id, lat, lng, max_attempts):
         args = {
-            'url': "https://api.telegram.org/bot%s/sendVenue".format(
+            'url': "https://api.telegram.org/bot{}/sendVenue".format(
                 bot_token),
             'payload': {
                 'chat_id': chat_id,
@@ -278,6 +313,7 @@ class TelegramAlarm(Alarm):
 
     # Send a payload to the webhook url
     def send_webhook(self, url, payload):
+        log.debug(url)
         log.debug(payload)
         resp = requests.post(url, json=payload, timeout=5)
         if resp.ok is True:
