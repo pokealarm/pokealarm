9d64a52e5ab6c1920e9e4d835765092189edb5b9
kvangent <kurtis.vangent@coyotes.usd.edu>
2018-01-17 20:20:04 -0800

    Disable web page preview.

--- PokeAlarm/Alarms/Telegram/TelegramAlarm.py
+++ PokeAlarm/Alarms/Telegram/TelegramAlarm.py
@@ -229,7 +229,8 @@ class TelegramAlarm(Alarm):
                 'chat_id': chat_id,
                 'text': message,
                 'parse_mode': 'Markdown',
-                'disable_web_page_preview': False,
+                'disable_web_page_preview': True
+                ,
                 'disable_notification': not notify
             }
         }
