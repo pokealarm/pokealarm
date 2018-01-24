4d0e8c4f718998eeaaf0d2a3ae6a34524625b702
Billy Bryant <3013565+billyjbryant@users.noreply.github.com>
2018-01-23 19:04:33 -0500

    Removing <boosted_weather_or_empty>
    <boosted_weather> never returns empty, so removing un-necessary <boosted_weather_or_empty>

--- PokeAlarm/Events/MonEvent.py
+++ PokeAlarm/Events/MonEvent.py
@@ -142,7 +142,6 @@ class MonEvent(BaseEvent):
             'weather_emoji': get_weather_emoji(self.weather_id),
             'boosted_weather_id': self.boosted_weather_id,
             'boosted_weather': boosted_weather_name,
-            'boosted_weather_or_empty': Unknown.or_empty(boosted_weather_name),
             'boosted_weather_emoji':
                 get_weather_emoji(self.boosted_weather_id),
 
