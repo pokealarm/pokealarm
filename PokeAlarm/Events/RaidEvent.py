b8e2a4bb2f81452537834dd528193f1411bb1977
darkelement1987 <jgernaat1987@gmail.com>
2019-10-13 10:50:05 +0200

    Update RaidEvent.py

--- PokeAlarm/Events/RaidEvent.py
+++ PokeAlarm/Events/RaidEvent.py
@@ -138,12 +138,14 @@ class RaidEvent(BaseEvent):
             'form': form_name,
             'form_or_empty': Unknown.or_empty(form_name),
             'form_id': self.form_id,
+            'form_id_2': "{:02d}".format(self.form_id),
             'form_id_3': "{:03d}".format(self.form_id),
 
             # Costume
             'costume': costume_name,
             'costume_or_empty': Unknown.or_empty(costume_name),
             'costume_id': self.costume_id,
+            'costume_id_2': "{:02d}".format(self.costume_id),
             'costume_id_3': "{:03d}".format(self.costume_id),
 
             # Location
