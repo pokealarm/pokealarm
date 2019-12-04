e2fc0c1af31a14cadf5fbed0015aaeb24ae66a24
Thorben Otto <thorben_otto@gmx.de>
2019-12-04 05:17:15 +0100

    New Lures and Invasions
    New invasion filter/alarm/rule type
    Support new lure types

--- PokeAlarm/Events/GruntEvent.py
+++ PokeAlarm/Events/GruntEvent.py
@@ -5,15 +5,18 @@ from datetime import datetime
 from PokeAlarm import Unknown
 from . import BaseEvent
 from PokeAlarm.Utils import get_gmaps_link, get_applemaps_link, \
-    get_waze_link, get_time_as_str, get_seconds_remaining, get_dist_as_str
+    get_waze_link, get_time_as_str, get_seconds_remaining, get_dist_as_str,\
+    get_type_emoji
+from PokeAlarm.Utilities.GruntUtils import get_grunt_gender, get_grunt_type_id,\
+    get_grunt_gender_sym
 
 
-class StopEvent(BaseEvent):
-    """ Event representing the discovery of a PokeStop. """
+class GruntEvent(BaseEvent):
+    """ Event representing the invasion of a PokeStop. """
 
     def __init__(self, data):
         """ Creates a new Stop Event based on the given dict. """
-        super(StopEvent, self).__init__('stop')
+        super(GruntEvent, self).__init__('grunt')
         check_for_none = BaseEvent.check_for_none
 
         # Identification
@@ -25,11 +28,18 @@ class StopEvent(BaseEvent):
             Unknown.REGULAR)
         self.stop_image = check_for_none(
             str, data.get('pokestop_url') or data.get('url'), Unknown.REGULAR)
-        self.lure_type_id = check_for_none(int, data.get('lure_id'), 0)
 
         # Time left
         self.expiration = datetime.utcfromtimestamp(
-            data.get('lure_expiration'))
+            data.get('incident_expiration',
+                     data.get('incident_expire_timestamp')))
+        self.type_id = check_for_none(
+            int, data.get('incident_grunt_type', data.get('grunt_type')),
+            0)
+        # Convert to standard grunt type ID
+        # self.type = int(math.floor(int(self.type_id) / 2)) * 2
+        self.gender_id = get_grunt_gender(self.type_id)
+        self.gender = get_grunt_gender_sym(self.gender_id)
 
         self.time_left = None
         if self.expiration is not None:
@@ -52,6 +62,7 @@ class StopEvent(BaseEvent):
         """ Return a dict with all the DTS for this event. """
         time = get_time_as_str(self.expiration, timezone)
         dts = self.custom_dts.copy()
+        type_name = locale.get_grunt_type_name(self.type_id)
         dts.update({
             # Identification
             'stop_id': self.stop_id,
@@ -59,9 +70,13 @@ class StopEvent(BaseEvent):
             # Details
             'stop_name': self.stop_name,
             'stop_image': self.stop_image,
-            'lure_type_id': self.lure_type_id,
-            'lure_type_id_3': "{:03}".format(self.lure_type_id),
-            'lure_type_name': locale.get_lure_type_name(self.lure_type_id),
+            'type_id': self.type_id,
+            'type_id_3': "{:03}".format(self.type_id),
+            'type_name': type_name,
+            'type_emoji': get_type_emoji(get_grunt_type_id(type_name)),
+            'gender_id': self.gender_id,
+            'gender': self.gender,
+            # ToDo: Add reward (should probably be sent via webhook)
 
             # Time left
             'time_left': time[0],
