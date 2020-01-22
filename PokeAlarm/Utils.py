555e25c53cb0b7d5b9fa2af450b1f9aa3f9a3920
Thorben Otto <thorben_otto@gmx.de>
2020-01-22 15:09:48 +0100

    Added new and improved existing verified dts

--- PokeAlarm/Utils.py
+++ PokeAlarm/Utils.py
@@ -358,6 +358,11 @@ def get_type_emoji(type_id):
         18: u'🌑'
     }.get(type_id, '')
 
+def get_verified_emoji(verified_id):
+    return {
+        0: u'❌',
+        1: u'✅',
+    }.get(verified_id, '❔')
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
