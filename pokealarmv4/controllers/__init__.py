7cac57d6d35de3245f560cb419f0069191923d91
kpvangent <10712294+kvangent@users.noreply.github.com>
2020-09-06 15:57:06 +0000

    Remove v4 module.

--- pokealarmv4/controllers/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-pokealarmv4.controllers
-~~~~~~~~~~~~~~~~
-
-This module contains controllers for maintaining access to different resources.
-"""
-from .file import EventFileController
-
-__all__ = [EventFileController]
