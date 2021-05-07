1ad55c11af5bcfd3d775e8bf016b8448c6b45f23
LegitDongo <9087537+LegitDongo@users.noreply.github.com>
2021-05-07 14:18:29 -0500

    Replace slacker API with official (#741)

--- PokeAlarm/Alarms/Slack/__init__.py
+++ PokeAlarm/Alarms/Slack/__init__.py
@@ -1,8 +1,8 @@
 try:
-    import slacker  # noqa F401
+    import slack  # noqa F401
 except ImportError:
     from PokeAlarm.Utils import pip_install
 
-    pip_install('slacker', '0.9.24')
+    pip_install('slackclient', '2.9.2')
 
 from .SlackAlarm import SlackAlarm  # noqa 401
