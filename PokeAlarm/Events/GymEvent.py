a1bffc04facdcb80a32dccc7fbc36a7a1142f67b
Pontiky <tortue.anthony@hotmail.fr>
2023-01-06 20:37:43 +0100

    RDM and MAD dts full support

--- PokeAlarm/Events/GymEvent.py
+++ PokeAlarm/Events/GymEvent.py
@@ -43,7 +43,9 @@ class GymEvent(BaseEvent):
         ).strip()
         self.gym_image = check_for_none(str, data.get("url"), Unknown.REGULAR)
         self.ex_eligible = check_for_none(
-            int, data.get("is_ex_raid_eligible"), Unknown.REGULAR
+            int,
+            data.get("is_ex_raid_eligible", data.get("ex_raid_eligible")),
+            Unknown.REGULAR,
         )
 
         # Gym Guards
@@ -55,8 +57,45 @@ class GymEvent(BaseEvent):
             if Unknown.is_not(self.slots_available)
             else Unknown.TINY
         )
+        self.guard_pokemon_id = check_for_none(
+            int, data.get("guard_pokemon_id"), Unknown.TINY
+        )  # RDM only
+        self.total_cp = check_for_none(
+            int, data.get("total_cp"), Unknown.TINY
+        )  # RDM only
+        self.in_battle = check_for_none(
+            int, data.get("is_in_battle", data.get("in_battle")), Unknown.TINY
+        )
+        self.power_up_points = check_for_none(
+            int, data.get("power_up_points"), Unknown.TINY
+        )  # RDM only
+        self.power_up_level = check_for_none(
+            int, data.get("power_up_level"), Unknown.TINY
+        )  # RDM only
+        self.power_up_end_timestamp = datetime.utcfromtimestamp(
+            data.get("power_up_end_timestamp", 0)
+        )  # RDM only
 
-        self.sponsor_id = check_for_none(int, data.get("sponsor"), Unknown.TINY)
+        # Sponsor
+        self.sponsor_id = check_for_none(
+            int, data.get("sponsor", data.get("sponsor_id")), Unknown.TINY
+        )
+        self.partner_id = check_for_none(
+            int, data.get("partner_id"), Unknown.TINY
+        )  # RDM only
+        self.ar_scan_eligible = check_for_none(
+            int,
+            data.get("is_ar_scan_eligible", data.get("ar_scan_eligible")),
+            Unknown.TINY,
+        )
+
+        # Misc
+        self.enabled = check_for_none(
+            int, data.get("enabled"), Unknown.TINY
+        )  # RDM only
+        self.last_modified = datetime.utcfromtimestamp(
+            data.get("last_modified", 0)
+        )  # RDM only
 
         self.name = self.gym_id
         self.geofence = Unknown.REGULAR
@@ -115,11 +154,21 @@ class GymEvent(BaseEvent):
                 # Guards
                 "slots_available": self.slots_available,
                 "guard_count": self.guard_count,
+                "guard_pokemon_id": self.guard_pokemon_id,
+                "total_cp": self.total_cp,
+                "in_battle": self.in_battle,
+                "power_up_points": self.power_up_points,
+                "power_up_level": self.power_up_level,
+                "power_up_end_timestamp": self.power_up_end_timestamp,
                 # Sponsor
                 "sponsor_id": self.sponsor_id,
                 "sponsored": self.sponsor_id > 0
                 if Unknown.is_not(self.sponsor_id)
                 else Unknown.REGULAR,
+                "partner_id": self.partner_id,
+                "ar_scan_eligible": self.ar_scan_eligible,
+                "enabled": self.enabled,
+                "last_modified": self.last_modified,
                 "current_timestamp_utc": datetime.utcnow(),
             }
         )
